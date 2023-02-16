eccac3380f92455015d875854ffe3aa67d275f2c
Hoffi <58910795+Hoffi-Flex@users.noreply.github.com>
2023-02-16 02:15:36 +0100

    Added Worker Thread with position simulator

--- server.js
+++ server.js
@@ -3,53 +3,79 @@ const app = express();
 app.use(express.json());
 const Redis = require("ioredis");
 const redis = new Redis();
+//.ENV
+require('dotenv').config();
+
+const { Worker } = require('worker_threads');
+
+//Worker Thread magic
+const worker = new Worker('./worker.js');
+
+worker.on('message', message => {
+  console.log('Worker message:', message);
+});
+
+worker.on('error', error => {
+  console.error('Worker error:', error);
+});
+
+worker.on('exit', code => {
+  console.log('Worker exit:', code);
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
 
-app.post("/rettungskraft", async (req, res) => {
+app.post("/rescueservice", async (req, res) => {
   const { name, lat, long } = req.body;
   if (!name || !lat || !long) {
     res.status(400).send({ error: "Fehlerhafte Anfrage!" });
   }
-  await redis.geoadd("rettungskraefte", long, lat, name);
+  await redis.geoadd("rescueservice", long, lat, name);
   res.status(200).send({ message: "Erfolgreich hinzugefügt!" });
 });
 
-app.get("/rettungskraft/:lat/:long/:radius", async (req, res) => {
+app.get("/rescueservice/:name", async (req, res) => {
+  const pos = await redis.geopos("rescueservice", req.params.name);
+  res.status(200).send({ position: pos[0].reverse() });
+});
+
+app.get("/rescueservice/:lat/:long/:radius", async (req, res) => {
   const { lat, long, radius } = req.params;
   if (!lat || !long || !radius) {
     res.status(400).send({ error: "Fehlerhafte Anfrage!" });
   }
   await redis.georadius(
-    "rettungskraefte",
+    "rescueservice",
     long,
     lat,
     radius,
     "km",
     "WITHDIST",
+    "WITHCOORD",
+    "ASC",
     function (err, reply) {
       if (err) {
         res.status(500).send({ error: "Ein Fehler ist aufgetreten!" });
       } else {
-        res.status(200).send({ locations: reply });
+        res.status(200).send({ locations: reply.map((item) => { return { name: item[0], distance: item[1], position: item[2].reverse() }}) });
       }
     }
   );
 });
 
-app.delete("/rettungskraft", async (req, res) => {
-    await redis.del("rettungskraefte");
+app.delete("/rescueservice", async (req, res) => {
+    await redis.del("rescueservice");
     res.status(200).send({ message: "Erfolgreich gelöscht!" });
     }
 );
 
-app.delete("/rettungskraft/:name", async (req, res) => {
+app.delete("/rescueservice/:name", async (req, res) => {
   const { name } = req.params;
   if (!name) {
     res.status(400).send({ error: "Fehlerhafte Anfrage!" });
   }
-  await redis.zrem("rettungskraefte", name);
+  await redis.zrem("rescueservice", name);
   res.status(200).send({ message: "Erfolgreich gelöscht!" });
 });
---
+++ worker.js
@@ -0,0 +1,41 @@
+const { parentPort } = require("worker_threads");
+const { faker } = require("@faker-js/faker");
+const { Redis } = require("ioredis");
+const redis = new Redis();
+//parentPort.postMessage(process.env.RESCUE_COUNT);
+let rescues = [];
+
+async function initialRescueService() {
+    parentPort.postMessage(`Started initial generation of ${process.env.RESCUE_COUNT} rescue services.`)
+    await redis.del("rescueservice");
+    for (let i = 0; i < process.env.RESCUE_COUNT; i++) {
+        const name = `Rescue Service ${i}`;
+        rescues.push(name);
+        const lat = faker.address.latitude(54.9079, 47.40724, 4);
+        const long = faker.address.longitude(14.98853, 5.98815, 4);
+        await redis.geoadd("rescueservice", long, lat, name);
+    }
+    parentPort.postMessage(`All initial ${process.env.RESCUE_COUNT} rescue services were successfully added.`);
+}
+
+async function startWorker() {
+    let active = true;
+    parentPort.postMessage(`Position simulatior started. With ${rescues.length/12000}`);
+    
+  while (active) {
+    //active = false;
+    const randomIndex = Math.floor(Math.random() * rescues.length);
+    const randomRescue = rescues[randomIndex];
+    const position = await redis.geopos("rescueservice", randomRescue);
+    const newPos = faker.address.nearbyGPSCoordinate([Number(position[0][1]), Number(position[0][0])], rescues.length/12000, true);
+    setTimeout(() => { const i = 99+1}, 100);
+    await redis.geoadd("rescueservice", newPos[1], newPos[0], randomRescue);
+  }
+}
+
+async function main() {
+  await initialRescueService();
+  await startWorker();
+}
+
+main();
